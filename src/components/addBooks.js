6b409a5a35c8de31d1af1dbe7cdb3ed903c78c77
richemonday@gmail.com <richemonday@gmail.com>
2022-06-23 20:50:49 +0100

    upaadted my addboks file

--- src/components/addBooks.js
+++ src/components/addBooks.js
@@ -1,58 +1,22 @@
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { useDispatch } from 'react-redux/es/exports';
-import { addBook } from '../redux/books/books';
+import React from 'react';
 
-const AddBooks = () => {
-  const initialState = {
-    title: '',
-    author: '',
-    category: '',
-  };
+const AddBook = () => (
+  <div>
+    <h2>Add New Book</h2>
+    <input type="text" placeholder="Book title" />
+    <form action="#">
+      <select name="books" id="books">
+        <option value="">Catefory</option>
+        <option value="biography">Biography</option>
+        <option value="romance">Romance</option>
+        <option value="action">Action</option>
+        <option value="health">Health</option>
 
-  const [inputState, setState] = useState(initialState);
+      </select>
 
-  const handleChange = (e) => {
-    setState({
-      ...inputState,
-      [e.target.name]: [e.target.value],
-    });
-  };
-
-  const dispatch = useDispatch();
-
-  const submitBook = (event) => {
-    event.preventDefault();
-    document.querySelector('form').reset();
-
-    const newBook = {
-      id: uuidv4(),
-      title: inputState.title[0],
-      author: inputState.author[0],
-      category: inputState.category,
-    };
-
-    dispatch(addBook(newBook));
-  };
-
-  return (
-    <form onSubmit={submitBook} className="input-book-container">
-      <h1>Add New Book</h1>
-      <div className="book-input">
-        <input type="text" placeholder="Book Title" name="title" onChange={handleChange} required />
-        <input type="text" placeholder="Book Author" name="author" onChange={handleChange} required />
-        <div className="form-wrapper">
-          <select name="category" id="categories" onChange={handleChange} required>
-            <option value="Undefine">Select a category</option>
-            <option value="Category 1">Category 1</option>
-            <option value="Category 2">Category 2</option>
-            <option value="Category 3">Category 3</option>
-          </select>
-          <button type="submit" id="add-btn" onClick={submitBook}>ADD BOOK</button>
-        </div>
-      </div>
+      <button type="submit"> Add Book</button>
     </form>
-  );
-};
+  </div>
+);
 
-export default AddBooks;
+export default AddBook;
