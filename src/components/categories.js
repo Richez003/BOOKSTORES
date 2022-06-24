89935f84d6962a3f57b2c38c60c4fec0c2bf78c1
richemonday@gmail.com <richemonday@gmail.com>
2022-06-24 05:56:05 +0100

    added a function to check status

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux/es/exports';
+import { checkStatus } from '../redux/categories/categories';
 
-const Category = () => (
-  <div className="buttonDiv">
-    <button className="catbtn" type="button">Check Status</button>
-  </div>
-);
+const Category = (props) => {
+  const id = props;
+  const dispatch = useDispatch();
+
+  const Status = () => {
+    dispatch(checkStatus(id));
+  };
+
+  return (
+    <div className="buttonDiv">
+      <button className="catbtn" type="button" onChange={Status}>Check Status</button>
+    </div>
+  );
+};
 
 export default Category;
